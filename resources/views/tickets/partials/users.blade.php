9851b098e4e6fee901cfc2ec3cc7b3107609c38e
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2020-12-20 18:10:50 +0100

    Update users.blade.php

--- resources/views/tickets/partials/users.blade.php
+++ resources/views/tickets/partials/users.blade.php
@@ -4,9 +4,7 @@
             <div class="card-body">
                 <div class="row justify-content-between align-items-center">
                     <div class="col-sm-4 col-md-4 col-lg-3 col-xl-2">
-                        <img class="rounded" height="48"
-                             src="https://avatars.r-services.eu/{{ $message->user->id }}">
-
+                        <img class="rounded" height="48" src="https://avatars.r-services.eu/{{ $message->user->id }}">
                     </div>
                     <div class="col-sm-8 col-md-8 col-lg-9 col-xl-10">
                         <div class="row">
@@ -14,8 +12,7 @@
                                 {{ $message->user->name }}
                             </div>
                             <div class="col-12 ">
-                                                <span
-                                                    class="text-muted">{{ $message->created_at->format(config('laravel-tickets.datetime-format')) }}</span>
+                                <span class="text-muted">{{ $message->created_at->format(config('laravel-tickets.datetime-format')) }}</span>
                             </div>
                         </div>
                     </div>
