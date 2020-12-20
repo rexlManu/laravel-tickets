24d047cc7b3562d15e510f497fecbb6c8125398b
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2020-12-20 17:39:28 +0100

    Fixed logic from Emmanuel Lampe

--- src/Traits/HasConfigModel.php
+++ src/Traits/HasConfigModel.php
@@ -9,11 +9,23 @@ trait HasConfigModel
 
     public function getKeyType()
     {
-        return 'string';
+        return config('laravel-tickets.models.key-type');
     }
 
     public function isIncrementing()
     {
-        return config('laravel-tickets.model.incrementing');
+        return config('laravel-tickets.models.incrementing');
     }
+    
+    public static function bootHasUuid()
+    {
+        if(config('laravel-tickets.models.uuid')) {
+            static::creating(function ($model) {
+                if (empty($model->id)) {
+                    $model->id = Uuid::uuid4();
+                }
+            });
+        }
+    }
+    
 }
