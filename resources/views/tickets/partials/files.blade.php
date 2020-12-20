4866ad3fe89885da71242e383be900eb10de4eca
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2020-12-20 18:09:32 +0100

    Update files.blade.php

--- resources/views/tickets/partials/files.blade.php
+++ resources/views/tickets/partials/files.blade.php
@@ -1,18 +1,18 @@
 <div style="height: 28.15vh; overflow-y: scroll">
     @foreach ($messages->get() as $message)
-        @foreach($message->uploads()->get() as $upload)
+        @foreach($message->uploads as $ticketUpload)
             <div class="card mt-2">
                 <div class="card-body">
                     <div class="row justify-content-between align-items-center">
                         <div class="col-12">
                             <div class="row">
                                 <div class="col-12">
-                                    <a href="{{ route('laravel-tickets.tickets.download', ['ticket' => $ticket, 'ticketUpload' => $upload]) }}">{{ basename($upload->path) }}</a>
+                                    <a href="{{ route('laravel-tickets.tickets.download', compact('ticket', 'ticketUpload')) }}">{{ basename($ticketUpload->path) }}</a>
                                 </div>
                                 <div class="col-12 ">
-                                                <span class="text-muted">
-                                                    {{ $message->user->name }} {{ $upload->created_at->format(config('laravel-tickets.datetime-format')) }}
-                                                </span>
+                                    <span class="text-muted">
+                                        {{ $message->user->name }} {{ $ticketUpload->created_at->format(config('laravel-tickets.datetime-format')) }}
+                                    </span>
                                 </div>
                             </div>
                         </div>
